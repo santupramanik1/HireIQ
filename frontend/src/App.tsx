b2bd5ea7fe54ec87c5ea22de7e85af043eac5c71
Santu pramanik <santu700141@gmail.com>
2026-04-08 01:43:15 +0530

    rearrange the component in ui folder

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,12 @@
+import {Route, Routes} from "react-router-dom";
 import LayoutPage from "./app/LayoutPage";
+import AuthDemoFlow from "./components/AuthDemoFlow";
 
 export const App = () => {
-    return <LayoutPage></LayoutPage>;
+    return (
+        <Routes>
+            <Route path="/" element={<LayoutPage />} />
+            <Route path="/login" element={<AuthDemoFlow/>} />
+        </Routes>
+    );
 };
