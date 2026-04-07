a110dd8fa1c2242daa670c35dc02fbf9d2971e2f
Santu pramanik <santu700141@gmail.com>
2026-04-07 21:35:53 +0530

    establish a connection to mongoDB database

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,18 +1,20 @@
 import dotenv from "dotenv";
-import express from "express";
+import express, { type Request, type Response } from "express";
+import { connDB } from "./config/db.js";
 
 dotenv.config();
 
 const app = express();
 
-app.get("/", (req, res) => {
+app.get("/", (req:Request, res:Response) => {
     return res.json({ success: "server setup" });
 });
 
 const PORT = process.env.PORT || 3000;
 
-console.log("PORT:", PORT);
 
+// Mongodb connection
+await connDB()
 app.listen(PORT, () => {
     console.log("Server is Running at PORT :", PORT);
 });
