e0a7aca1add13d4211824480d74ae93ebfcf02ce
Santu pramanik <santu700141@gmail.com>
2026-07-05 22:27:56 +0530

    fix: main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,7 @@ import './index.css';
 import { App } from './App.tsx';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext.tsx';
+// main
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
